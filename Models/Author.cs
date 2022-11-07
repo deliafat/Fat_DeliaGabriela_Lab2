ac473dd9813905529d70da2b37400fe90b81cbc2
Delia <delia.fat13@gmail.com>
2022-11-07 02:51:44 +0200

    Laborator 4

--- Models/Author.cs
+++ Models/Author.cs
@@ -1,12 +1,22 @@
-﻿namespace Fat_DeliaGabriela_Lab2.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Fat_DeliaGabriela_Lab2.Models
 {
     public class Author
     {
         public int Id { get; set; }
-        public string FirstName   { get; set; }
+        public string FirstName { get; set; }
         public string LastName { get; set; }
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
 
-        public string FullName { get; set; }
-        public ICollection<Book>? Books { get; set; }
+        public ICollection<Book>? Books { get; set; } 
     }
 }
--- Pages/Books/Edit.cshtml.cs
+++ Pages/Books/Edit.cshtml.cs
@@ -32,10 +32,11 @@ namespace Fat_DeliaGabriela_Lab2.Pages.Books
             }
 
             Book = await _context.Book
-                .Include(b => b.Publisher)
-                .Include(b => b.BookCategories).ThenInclude(b => b.Category)
-                .AsNoTracking()
-                .FirstOrDefaultAsync(m => m.Id == id);
+              .Include(b => b.Publisher)
+              .Include(b => b.Author)
+              .Include(b => b.BookCategories).ThenInclude(b => b.Category)
+              .AsNoTracking()
+              .FirstOrDefaultAsync(m => m.Id == id);
 
             var book = await _context.Book.FirstOrDefaultAsync(m => m.Id == id);
             if (Book == null)
@@ -64,6 +65,7 @@ selectedCategories)
             }
             var bookToUpdate = await _context.Book
             .Include(i => i.Publisher)
+            .Include(i => i.Author)
             .Include(i => i.BookCategories)
             .ThenInclude(i => i.Category)
             .FirstOrDefaultAsync(s => s.Id == id);
